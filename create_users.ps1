1b30e25442f77a0a28108841a66169c4e9de2cac
lemmusm <lemmusm@gmail.com>
2021-03-31 16:33:00 -0600

    Se crea script

---
+++ create_users.ps1
@@ -0,0 +1,22 @@
+# Desde Powershell ejecuta: & "./create_users.ps1", en tu carpeta raíz deberas tener tu script y el archivo csv
+# y las columnas deberan coincidir con los valores que le asignas a tus variables.
+
+#Variable para el id del user pool de cognito
+$poolId = ""
+
+# Se lee el archivo csv que contiene los datos
+$users_list = Import-Csv ./usuarios.csv
+
+# Se recorre las filas del csv y se almacenan los valores en las variables para usarlas en el comando de aws
+foreach ($user in $users_list) {
+    $idUsuario  =  $user.idUsuario
+    $idRol      =  $user.idRol
+    $nombre     =  $user.nombre
+    $usuario    =  $user.usuario
+    $password   =  $user.password
+    $correo     =  $user.correo
+    #Write-Host "aws cognito-idp admin-create-user --user-pool-id $poolId --username $usuario --temporary-password $password --user-attributes Name=email,Value=$correo Name=phone_number,Value="+521234567890" Name=email_verified,Value=True Name=phone_number_verified,Value=True Name=custom:idUsuario,Value=$idUsuario Name=custom:idRol,Value=$idRol --message-action SUPPRESS"
+
+    #Ejecuta el comando de aws para crear el usuario en cognito
+    aws cognito-idp admin-create-user --user-pool-id $poolId --username $usuario --temporary-password $password --user-attributes Name=name,Value=$nombre Name=email,Value=$correo Name=phone_number,Value="+521234567890" Name=email_verified,Value=True Name=phone_number_verified,Value=True Name=custom:idUsuario,Value=$idUsuario Name=custom:idRol,Value=$idRol --message-action SUPPRESS
+}
