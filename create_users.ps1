4bb4f08f5f82efddb45b08e193caeeb596b6d523
Manuel Hernandez Lemus <lemmusm@gmail.com>
2021-04-02 11:54:33 -0600

    Se eliminan comentarios

--- create_users.ps1
+++ create_users.ps1
@@ -1,6 +1,3 @@
-# Desde Powershell ejecuta: & "./create_users.ps1", en tu carpeta raíz deberas tener tu script y el archivo csv
-# y las columnas deberan coincidir con los valores que le asignas a tus variables.
-
 #Variable para el id del user pool de cognito
 $poolId = ""
 
@@ -16,7 +13,6 @@ foreach ($user in $users_list) {
     $password = $user.password
     $correo = $user.correo
 
-    #WARNING: El auto-formatter de vscode agrega espacios en el siguiente espacio, verificar que esto no suceda, de lo contrario se obtendran errores
     #Ejecuta el comando de aws para crear el usuario en cognito.
     aws cognito-idp admin-create-user --user-pool-id $poolId --username $usuario --temporary-password $password --user-attributes Name=name,Value=$nombre Name=email,Value=$correo Name=phone_number,Value="+521234567890" Name=email_verified,Value=True Name=phone_number_verified,Value=True Name=custom:idUsuario,Value=$idUsuario Name=custom:idRol,Value=$idRol --message-action SUPPRESS
 }
